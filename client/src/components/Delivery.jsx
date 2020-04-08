a6a0ec0ff65323a3ae9e8160d8ae75066593d01d
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-04-08 09:07:45 -0400

    added font awesome icons and formated delivery page

--- client/src/components/Delivery.jsx
+++ client/src/components/Delivery.jsx
@@ -3,27 +3,28 @@ import { Link } from 'react-router-dom'
 import takeout from '../images/takeout.png'
 import delivery from '../images/delivery.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils, faCar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Delivery(props) {
 
   return (
     <div>
-      <div>
-        <Link exact activeClassName="active" to="/cart">
-          <img onClick={() => props.setDelivery("Delivery")} src={delivery} alt="Delivery" />
-        </Link>
+      <div className="delivery-container">
+        <div>
+          <Link exact activeClassName="active" to="/cart">
+            <FontAwesomeIcon style={{ color: "#000000" }} onClick={() => props.setDelivery("Delivery")} size="10x" icon={faCar} />
+          </Link>
+          <div className="delivery-option">Delivery</div>
+        </div>
 
-        <div>Delivery</div>
-        <Link exact activeClassName="active" to="/cart">
-          <img onClick={() => props.setDelivery("Take-out")} src={takeout} alt="TakeOut" />
-        </Link>
-        
-        <div>Take Out</div>
-
-        <FontAwesomeIcon icon={faBars} />
+        <div>
+          <Link exact activeClassName="active" to="/cart">
+            <FontAwesomeIcon style={{ color: "#000000" }} onClick={() => props.setDelivery("Take-out")} size="10x" icon={faUtensils} />
+          </Link>
+      <div className="delivery-option">Take Out</div>
       </div>
 
+      </div>
     </div>
   );
 }
